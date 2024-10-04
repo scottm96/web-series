eea61dddf34e3262423da7175944418ad69a9293
Maxwell Williams <awoonor1z@gmail.com>
2024-10-04 15:40:07 +0000

    checked property
    using the checked property in js to execute a fuction on a checkbox or radiobutton of an html page

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
-//If statements
-
-const input = document.getElementById("age");
-const submit = document.getElementById("submit");
-const result = document.getElementById("result");
+const mycheck = document.getElementById("mycheck");
+const visa = document.getElementById("visa");
+const mastercard = document.getElementById("mastercard");
+const paypal = document.getElementById("paypal");
+const submit = document.getElementById("submit")
+const subresult = document.getElementById("subresult")
+const payresult = document.getElementById("payresult")
 
 
 submit.onclick = function(){
-    let age = input.value;
-    age = Number(age)
-
-    if (age >= 100){
-        result.textContent = "you are too old to enter this site"
-    }
-    else if (age == 0){
-        result.textContent = "you were just born "
-        
-    }
-    else if (age >= 18){
-        result.textContent = "you are old enough to enter this site"
-        
-    }
-    else if (age < 0){
-        result.textContent = "your age cannot be less than 0"
-        
+    if (mycheck.checked){
+        subresult.textContent = `You are subscribed`
     }
     else{
-        result.textContent = "you're not old enough to enter this site"
-        
+        subresult.textContent = `You are not subscribed`
     }
-}
 
+
+    if (visa.checked){
+        payresult.textContent = `You have selected visa payment`
+    }
+    else if (mastercard.checked){
+        payresult.textContent = `You have selected mastercard payment`
+    }
+    else if (paypal.checked){
+        payresult.textContent = `You have selected paypal payment`
+    }
+    else{
+        payresult.textContent = `You have to select a payment method`
+    }
+
+}
