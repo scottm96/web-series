af2951bea3bf4648ed0d4a300cd4b9c82b66519a
Maxwell Williams <awoonor1z@gmail.com>
2024-10-04 10:17:36 +0000

    working with variables
    declaration and assignments of strings and numbers. displaying variables on web page

--- index.js
+++ index.js
@@ -1,4 +1,29 @@
 //window.alert(`This is an alert`)
 
-document.getElementById("myh1").textContent = `hello`;
+/* document.getElementById("myh1").textContent = `hello`;
 document.getElementById("myp").textContent = `I like pizza`
+*/
+
+/* let age = 25;
+let price = 9.99;
+
+
+
+console.log(`you are ${age} years old`);
+console.log(`The book cost $${price}`)
+
+console.log(typeof (price))
+
+booleans
+let online = true;
+
+console.log(typeof online)
+*/
+
+let fullname = "Scott Spag"
+let age = 35
+let inschool = true
+
+document.getElementById("p1").textContent = `Your name is {fullname}`;
+document.getElementById("p2").textContent = `you are ${age} years old`;
+document.getElementById("p3").textContent = `Enrolled: ` + inschool;
