5a2589abef7e4eda9bb06ab74ce5b560b943496b
Maxwell Williams <awoonor1z@gmail.com>
2024-10-04 15:48:42 +0000

    a ternary operator in javascript
    a shortcut to if statements

--- index.js
+++ index.js
@@ -1,32 +1,13 @@
-const mycheck = document.getElementById("mycheck");
-const visa = document.getElementById("visa");
-const mastercard = document.getElementById("mastercard");
-const paypal = document.getElementById("paypal");
-const submit = document.getElementById("submit")
-const subresult = document.getElementById("subresult")
-const payresult = document.getElementById("payresult")
+//ternary operator
+
+/*let age = 2 ;
+
+let message = age >= 18 ? "you are an adult" : "you are a minor"
+
+console.log(message)*/
 
 
-submit.onclick = function(){
-    if (mycheck.checked){
-        subresult.textContent = `You are subscribed`
-    }
-    else{
-        subresult.textContent = `You are not subscribed`
-    }
+let purchaseamnt = 15;
+let discount = purchaseamnt >= 100 ? 10 : 0;
 
-
-    if (visa.checked){
-        payresult.textContent = `You have selected visa payment`
-    }
-    else if (mastercard.checked){
-        payresult.textContent = `You have selected mastercard payment`
-    }
-    else if (paypal.checked){
-        payresult.textContent = `You have selected paypal payment`
-    }
-    else{
-        payresult.textContent = `You have to select a payment method`
-    }
-
-}
+console.log(`Your total is $${purchaseamnt - (purchaseamnt * discount/100)}`)
