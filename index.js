b716c2d53832ded2ddb4906af79155dcbd9ad8ad
Maxwell Williams <awoonor1z@gmail.com>
2024-10-04 10:29:11 +0000

    working with arithmetic operators
    refer to comments in code

--- index.js
+++ index.js
@@ -1,29 +1,12 @@
-//window.alert(`This is an alert`)
+let students = 30;
 
-/* document.getElementById("myh1").textContent = `hello`;
-document.getElementById("myp").textContent = `I like pizza`
-*/
+//students =  students / 2;
+//students =  students + 2;
+//students =  students * 2;
+//students =  students - 2;
+//students =  students ** 2;
+//students =  students % 7;
 
-/* let age = 25;
-let price = 9.99;
+students += 4
 
-
-
-console.log(`you are ${age} years old`);
-console.log(`The book cost $${price}`)
-
-console.log(typeof (price))
-
-booleans
-let online = true;
-
-console.log(typeof online)
-*/
-
-let fullname = "Scott Spag"
-let age = 35
-let inschool = true
-
-document.getElementById("p1").textContent = `Your name is {fullname}`;
-document.getElementById("p2").textContent = `you are ${age} years old`;
-document.getElementById("p3").textContent = `Enrolled: ` + inschool;
+console.log(students)
