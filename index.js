a2bb4a6ff30b0aaf9b14bbf07fe825f35e0f4c35
Maxwell Williams <awoonor1z@gmail.com>
2024-10-06 22:11:53 +0000

    while loops and do while loops

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
-// strict equality === values and datatype
+//while loop 
 
-const PI = 3.14;
-
-if (PI === "3.14"){
-    console.log("that is pi")
-}
-else{
-    console.log("that is not pi")
+//let username = ""
+/*
+while(username === "" || username === null){
+    username = window.prompt("Enter your Name");
 }
 
-if (PI !== "3.14"){
-    console.log("that is not pi")
-}
-else{
-    console.log("that is pi")
+console.log(`Hello  ${username}`)
+*/
+
+let loggedIn = false;
+let username;
+let password;
+
+while(!loggedIn){
+    username = window.prompt("Enter your username")
+    password = window.prompt("Enter your password")
+
+    if (username === "user1" && password === "pass0"){
+        loggedIn = true
+        console.log(`Welcome ${username}`)
+    }
+    else{
+        console.log("Invalid credentials")
+    }
+
 }
