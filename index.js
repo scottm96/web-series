c7e6fbc756ab34b99df45c471d09d7e0ff015001
Maxwell Williams <awoonor1z@gmail.com>
2024-10-08 16:24:54 +0000

    a number guessing game using javascript window alerts and if statements

--- index.js
+++ index.js
@@ -1,15 +1,36 @@
-//for loop - repeat some code for  a certain number of times
+//number guessing game using js
 
-for(let i=10; i > 0; i--){
-    console.log(i);
-}
-console.log("Happy New Year")
+const min = 1;
+const max = 100;
+const answer = Math.floor(Math.random() * (max - min ) + min);
 
-for(let i = 1; i <= 20; i++){
-    if (i == 13){
-        break;
+let attempts = 0;
+let guess;
+let running = true;
+
+while (running){
+
+    guess = window.prompt(`Guess a number between ${min} and ${max}`)
+    guess = Number(guess)
+
+    if(isNaN(guess)){
+        window.alert("please enter a  valid number");
+    }
+    else if(guess < min || guess > max){
+        window.alert("Please enter a valid number");
     }
     else{
-        console.log(i)
+        attempts++;
+        if(guess < answer){
+            window.alert("Too low, try again")
+        }
+        else if (guess > answer){
+            window.alert("Too high, try again")
+        }
+        else{
+            window.alert(`Correct! it took you ${attempts} attemps`);
+            running = flase
+        }
     }
+    
 }
