0b408f9ad8b59e08ae98c8754f6e0859459a3c3b
Maxwell Williams <awoonor1z@gmail.com>
2024-10-26 16:02:07 +0000

    foreach method
    array.foreach(callback)

--- index.js
+++ index.js
@@ -1,19 +1,45 @@
-//callbacks - a function that is passed as an 
-//            argument to another function to guarantee 
-//           the function is called only after the main fuction has been executed
+//forEach method
 
 
-sum(displyPage, 3, 4)
+let numbers = [1, 2, 3, 4, 5]
 
-function sum(callback, x,y){
-    let result = x + y;
-    callback(result);
+
+numbers.forEach(square);
+numbers.forEach(display);
+
+
+function display(element){
+    console.log(element)
 }
 
-function displayconsole(result){
-    console.log(result)
+function double(element, index, array){
+    array[index] = element * 2;
 }
 
-function displyPage(result){
-    document.getElementById("myh1").textContent = result
+function triple(element, index, array){
+    array[index] = element * 3;
 }
+
+function square(element, index, array){
+    array[index] = element ** 2;
+}
+
+
+
+let fruits = ['apple', 'orange', 'banana'];
+
+fruits.forEach(capitalize)
+fruits.forEach(display)
+
+function display(element){
+    console.log(element);
+}
+
+function toUpper(element, index, array){
+    array[index] = element.toUpperCase();
+}
+
+function capitalize(element, index, array){
+    array[index] = element.charAt(0).toUpperCase() + element.slice(1)
+}
+
