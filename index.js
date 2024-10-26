e591f7ad6848741631c5c50cdf2549cb4467a80f
Maxwell Williams <awoonor1z@gmail.com>
2024-10-26 16:15:16 +0000

    .map method, just like foreach but returns a new array

--- index.js
+++ index.js
@@ -1,45 +1,31 @@
-//forEach method
+// .map()
 
+const numbers = [1, 2, 3, 4, 5];
 
-let numbers = [1, 2, 3, 4, 5]
+const squares = numbers.map(square);
 
+console.log(squares)
 
-numbers.forEach(square);
-numbers.forEach(display);
-
-
-function display(element){
-    console.log(element)
+function square(element){
+    return Math.pow(element, 2)
 }
 
-function double(element, index, array){
-    array[index] = element * 2;
-}
+const students = ['solar', 'annie', 'parasoul', 'eliza']
+const studentsup = students.map(upper)
 
-function triple(element, index, array){
-    array[index] = element * 3;
-}
+console.log(studentsup)
 
-function square(element, index, array){
-    array[index] = element ** 2;
+
+function upper(element){
+    return element.toUpperCase();
 }
 
 
+const dates = ['2024-1-23', '2003-8-12', '2014-5-28'];
+const formattedDates = dates.map(formatDates)
 
-let fruits = ['apple', 'orange', 'banana'];
-
-fruits.forEach(capitalize)
-fruits.forEach(display)
-
-function display(element){
-    console.log(element);
+console.log(formattedDates)
+function formatDates(element){
+    const parts = element.split('-')
+    return `${parts[1]}/${parts[2]}/${parts[0]}`
 }
-
-function toUpper(element, index, array){
-    array[index] = element.toUpperCase();
-}
-
-function capitalize(element, index, array){
-    array[index] = element.charAt(0).toUpperCase() + element.slice(1)
-}
-
