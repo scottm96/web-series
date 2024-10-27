908e4c60ee2af8295fcf2b7a4e541953dd85b138
Maxwell Williams <awoonor1z@gmail.com>
2024-10-27 23:39:05 +0000

    arrow functions

--- index.js
+++ index.js
@@ -11,10 +11,13 @@ setTimeout(() => console.log('hello'), 5000)
 
 const numbers = [1, 2, 3, 4, 5, 6]
 const squares = numbers.map((element) => Math.pow(element, 2))
+const cubes = numbers.map((element) => Math.pow(element, 3))
 const total = numbers.reduce((accumulator, element) => accumulator + element)
+const maximum = numbers.reduce((accumulator, element) => Math.max(accumulator, element))
 const evennum = numbers.filter((element) => element % 2 === 0)
 
 console.log(evennum)
 console.log(squares)
 console.log(total)
+console.log(maximum)
 
