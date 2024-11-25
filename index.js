3b35a8809ccf8ac481d07aa56df57a63d2e479a9
Maxwell Williams <awoonor1z@gmail.com>
2024-11-25 20:22:57 +0000

    try and catch to handle errors

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
-function func1(callback){
-    setTimeout(() => {console.log('Task 1');
-                      callback()}, 3000);
+//errors
+
+try{
+    const dividend = Number(window.prompt("Enter a dividend: "));
+    const divisor = Number(window.prompt("Enter a divisor: "));
+
+
+    if(divisor == 0){
+        throw new Error("You cannot divide by zero")
+    }
+    if(isNaN(dividend) || isNaN(divisor)){
+        throw new Error("value must be a number")
+    }
+
+    const result = dividend / divisor;
+    console.log(result);
+}
+catch(error){
+    console.error(error);
 }
 
-function func2(){
-    console.log("Task 2");
-    console.log("Task 3");
-    console.log("Task 4");
-}
-
-func1(func2)
+console.log('you have reaced the end of the program')
