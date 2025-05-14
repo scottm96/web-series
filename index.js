885427251cb9a2ad153559e40d710e666337b332
Maxwell Williams <awoonor1z@gmail.com>
2025-05-14 04:26:35 +0000

    rock paper scissors game --complete with score feature

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const choices = ['rock', 'paper', 'scissors'];
 const playerDisplay = document.getElementById('playerDisplay');
 const computerDisplay = document.getElementById('computerDisplay');
 const resultDisplay = document.getElementById('resultDisplay');
+const playerScoreDisplay = document.getElementById('playerScoreDisplay');
+const computerScoreDisplay = document.getElementById('computerScoreDisplay');
+
+
+let computerScore = 0
+let playerScore = 0
+
 
 function playGame(playerChoice){
 
@@ -31,13 +38,19 @@ function playGame(playerChoice){
     
     resultDisplay.classList.remove("greenText", "redText")
     resultDisplay.textContent = result;
+    
+    
 
     switch(result){
         case "YOU WIN":
             resultDisplay.classList.add("greenText")
+            playerScore ++;
+            playerScoreDisplay.textContent = playerScore
             break
         case "YOU LOSE":
             resultDisplay.classList.add("redText")
+            computerScore ++;
+            computerScoreDisplay.textContent = computerScore
             break
     }
 }
