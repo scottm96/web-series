13776609dac373f48f7c080de676e0cd5919f190
Maxwell Williams <awoonor1z@gmail.com>
2025-05-14 04:02:44 +0000

    rock paper scissors game --complete

--- index.js
+++ index.js
@@ -26,5 +26,18 @@ function playGame(playerChoice){
         }
     }
 
-    playerDisplay.textContent = `Player: ${playerChoice}`
+    playerDisplay.textContent = `PLAYER: ${playerChoice}`;
+    computerDisplay.textContent = `Computer: ${computerChoice}`
+    
+    resultDisplay.classList.remove("greenText", "redText")
+    resultDisplay.textContent = result;
+
+    switch(result){
+        case "YOU WIN":
+            resultDisplay.classList.add("greenText")
+            break
+        case "YOU LOSE":
+            resultDisplay.classList.add("redText")
+            break
+    }
 }
