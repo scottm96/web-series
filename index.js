65781b59bfa4ace4681207dd482652065b6f1d45
Maxwell Williams <awoonor1z@gmail.com>
2025-05-15 06:56:07 +0000

    testing out callback hell

--- index.js
+++ index.js
@@ -1,56 +1,27 @@
-const choices = ['rock', 'paper', 'scissors'];
-const playerDisplay = document.getElementById('playerDisplay');
-const computerDisplay = document.getElementById('computerDisplay');
-const resultDisplay = document.getElementById('resultDisplay');
-const playerScoreDisplay = document.getElementById('playerScoreDisplay');
-const computerScoreDisplay = document.getElementById('computerScoreDisplay');
+//callback hell
 
-
-let computerScore = 0
-let playerScore = 0
-
-
-function playGame(playerChoice){
-
-    const computerChoice = choices[Math.floor(Math.random() * 3)]
-    let result = "";
-
-    if(playerChoice === computerChoice){
-        result = "IT'S A TIE"
-
-    }
-    else{
-        switch(playerChoice){
-            case "rock" :
-               result = (computerChoice === "scissors") ? "YOU WIN" : "YOU LOSE";
-               break;
-            case "paper" :
-               result = (computerChoice === "rock") ? "YOU WIN" : "YOU LOSE";
-               break;
-            case "scissors" :
-               result = (computerChoice === "paper") ? "YOU WIN" : "YOU LOSE";
-               break;
-        }
-    }
-
-    playerDisplay.textContent = `PLAYER: ${playerChoice}`;
-    computerDisplay.textContent = `Computer: ${computerChoice}`
-    
-    resultDisplay.classList.remove("greenText", "redText")
-    resultDisplay.textContent = result;
-    
-    
-
-    switch(result){
-        case "YOU WIN":
-            resultDisplay.classList.add("greenText")
-            playerScore ++;
-            playerScoreDisplay.textContent = playerScore
-            break
-        case "YOU LOSE":
-            resultDisplay.classList.add("redText")
-            computerScore ++;
-            computerScoreDisplay.textContent = computerScore
-            break
-    }
+function task1(callback){
+    setTimeout( ()=> {console.log("task 1 is complete")
+        callback()
+    }, 3000)
 }
+
+function task2(callback){
+    setTimeout( ()=> {console.log("task 2 is complete")
+        callback()
+    }, 2000)
+}
+
+function task3(callback){
+    setTimeout( ()=> {console.log("task 3 is complete")
+        callback()
+    }, 1000)
+}
+
+task1(()=>{
+    task2(()=>{
+        task3(()=>{
+            task1()
+        })
+    })
+})
