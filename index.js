6fa024f21f55cfd259fe32ae1991d96cee99fa70
Maxwell Williams <awoonor1z@gmail.com>
2025-05-18 12:46:05 +0000

    promises - testing out resolve and reject

--- index.js
+++ index.js
@@ -5,7 +5,15 @@ function walkDog(){
 
     return new Promise((resolve, reject) => {
         setTimeout(() => {
-            resolve("You walk the Dog")
+
+            const dogWalked = true
+
+            if(dogWalked){
+                resolve("You walk the Dog")
+            }
+            else{
+                reject("You did not wal the dog")
+            }
         }, 2000);
     })
 }
@@ -15,7 +23,16 @@ function cleanKitchen(){
 
     return new Promise((resolve, reject) =>{
         setTimeout(() => {
-            resolve("You clean the kitchen")
+            
+            const kitchenCleaned  = false
+
+            if(kitchenCleaned){
+                resolve("You clean the kitchen")
+            }
+            else{
+                reject("You did not clean the kitchen")
+            }
+            
         }, 3000);
     })
 }
@@ -24,12 +41,22 @@ function takeOutTrash(){
 
 
     return new Promise((resolve, reject) => {
-    setTimeout(() => {
-        resolve("You take out the trash")
-    }, 1000);
+
+        setTimeout(() => {
+            const trashTakenOut = true;
+
+            if(trashTakenOut){
+                resolve("You did not take out the trash")
+            }
+            else{
+                reject("You did not take out the trash")
+            }
+        }, 1000);
+        
     })
 }
 
 walkDog().then((value) => {console.log(value); return cleanKitchen()})
          .then((value) => {console.log(value); return takeOutTrash()})
          .then((value) => {console.log(value); console.log("you completed all the chores")})
+         .catch((error) => console.error(error))
