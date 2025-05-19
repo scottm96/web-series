b802562ed48b68ae5aee8d9843d9d741363b3cdc
Maxwell Williams <awoonor1z@gmail.com>
2025-05-19 23:56:09 +0000

    JSON files- intro to fetch method

--- index.js
+++ index.js
@@ -1,34 +1,7 @@
 //JS Object Notation stringify and parse
 
-const names = ["squidward", "king", "chuck", "patrick"]
 
-const jsonString = JSON.stringify(names)
-
-console.log(jsonString)
-
-
-
-const people = [{
-    "name" : "squidward",
-    "age" : 36,
-    "isEmployed" : true
-},
-{
-    "name" : "patrick",
-    "age" : 29,
-    "isEmployed" : false
-},
-{
-    "name" : "king",
-    "age" : 36,
-    "isEmployed" : true
-}]
-
-const jsonPeople = JSON.stringify(people)
-
-console.log(jsonPeople)
-console.log(typeof(jsonPeople))
-
-
-console.log(JSON.parse(jsonPeople))
-console.log(typeof(JSON.parse(jsonPeople)))
+fetch("people.json")
+    .then( (response) => response.json())
+    .then( (values) => values.forEach(value => {console.log(value)}))
+    .catch( (error) => console.error(errors))
