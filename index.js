4c08d266e0a733c4838e5ee6ec09147e8ee22de2
Maxwell Williams <awoonor1z@gmail.com>
2025-05-21 02:29:07 +0000

    fetching the sprite of a pokemon per user request and display on screen

--- index.js
+++ index.js
@@ -2,24 +2,30 @@
 //  used to interact with api
 //fetch(url,{options})
 
+
 async function fetchData() {
 
     try{
 
-        const response = await fetch("https://pokeapi.co/api/v2/pokemon/gengar")
+        const pokemonName = document.getElementById("pokemonName").value.toLowerCase()
+
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
+        
 
         if(!response.ok){
-            throw new Error("cannot retrieve data")
+            throw new Error("could not retrieve data")
         }
 
         const data = await response.json()
-        console.log(data)
+        const pokemonSprite = data.sprites['front_default'];
+        const imgE = document.getElementById("pokemonSprite");
+
+        imgE.src = pokemonSprite;
+        imgE.style.display = 'block';
 
     }
-    catch(e){
-        console.log(e)
+    catch(error){
+        console.log(error)
     }
     
 }
-
-fetchData()
